f9da8ec5439c5abea9fabd73e78f93de0daaf80c
n.alipour777@yahoo.com <n.alipour777@yahoo.com>
2023-07-27 13:06:31 +1000

    style for sign up form

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,42 +1,9 @@
-/*
-import React from 'react';
-//import { ReactDOM } from 'react-dom';
-import '../styles/Home.css';
-
-const logoSrc =
-  'https://upload.wikimedia.org/wikipedia/commons/thumb/a/a7/React-icon.svg/1280px-React-icon.svg.png';
-
-
-
-const Signup = () => {
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logoSrc} className="App-logo" alt="logo" />
-        <p>
-         <code>This is signup</code>!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Built with React
-        </a>
-      </header>
-    </div>
-  );
-};
-export default Signup;
-*/
-
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { useMutation } from '@apollo/client';
 import { ADD_PROFILE } from '../utils/mutations';
 import Auth from '../utils/auth';
+import '../styles/Signup.css';
 
 const Signup = () => {
   const [formState, setFormState] = useState({
@@ -72,10 +39,10 @@ const Signup = () => {
   };
 
   return (
-    <main>
-      <div>
+    <main className= "page">
+      <div className="form"> 
         <div>
-          <h4>Sign Up</h4>
+          <h2>Sign Up</h2>
           <div>
             {data && data.addProfile ? (
               <p>
@@ -85,6 +52,7 @@ const Signup = () => {
             ) : (
               <form onSubmit={handleFormSubmit}>
                 <input
+                  className="signup-input" 
                   placeholder="Your username"
                   name="username"
                   type="text"
@@ -92,6 +60,7 @@ const Signup = () => {
                   onChange={handleChange}
                 />
                 <input
+                  className="signup-input" 
                   placeholder="Your email"
                   name="email"
                   type="email"
@@ -99,13 +68,18 @@ const Signup = () => {
                   onChange={handleChange}
                 />
                 <input
-                  placeholder="******"
+                  className="signup-input" 
+                  placeholder="Your password"
                   name="password"
                   type="password"
                   value={formState.password}
                   onChange={handleChange}
                 />
-                <button style={{ cursor: 'pointer' }} type="submit">
+                <button
+                  className="signup-button" 
+                  style={{ cursor: 'pointer' }}
+                  type="submit"
+                >
                   Submit
                 </button>
               </form>
@@ -123,105 +97,3 @@ export default Signup;
 
 
 
-
-/*
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-
-import { useMutation } from '@apollo/client';
-import { ADD_PROFILE } from '../utils/mutations';
-
-import Auth from '../utils/auth';
-
-const Signup = () => {
-  const [formState, setFormState] = useState({
-    username: '',
-    email: '',
-    password: '',
-  });
-  const [addProfile, { error, data }] = useMutation(ADD_PROFILE);
-
-  // update state based on form input changes
-  const handleChange = (event) => {
-    const { name, value } = event.target;
-
-    setFormState({
-      ...formState,
-      [name]: value,
-    });
-  };
-
-  // submit form
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-    console.log(formState);
-
-    try {
-      const { data } = await addProfile({
-        variables: { ...formState },
-      });
-
-      Auth.login(data.addProfile.token);
-    } catch (e) {
-      console.error(e);
-    }
-  };
-
-  return (
-    <main >
-        <h4 >Sign Up</h4>
-          <div >
-            {data ? (
-              <p>
-                Success! You may now head{' '}
-                <Link to="/">back to the homepage.</Link>
-              </p>
-            ) : (
-              <form onSubmit={handleFormSubmit}>
-                <input
-                  
-                  placeholder="Your username"
-                  name="username"
-                  type="text"
-                  value={formState.username}
-                  onChange={handleChange}
-                />
-                <input
-                  
-                  placeholder="Your email"
-                  name="email"
-                  type="email"
-                  value={formState.email}
-                  onChange={handleChange}
-                />
-                <input
-                  
-                  placeholder="******"
-                  name="password"
-                  type="password"
-                  value={formState.password}
-                  onChange={handleChange}
-                />
-                <button
-                  
-                  style={{ cursor: 'pointer' }}
-                  type="submit"
-                >
-                  Submit
-                </button>
-              </form>
-            )}
-
-            {error && (
-              <div>
-                {error.message}
-              </div>
-            )}
-        </div>
-    </main>
-  );
-};
-
-export default Signup;
-
-*/
